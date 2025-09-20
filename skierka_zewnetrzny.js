66ef2a8ba9de8aefc05219366041ef8bfad529c9
Adrian1811 <adrianskierka08@wp.pl>
2025-09-20 16:33:49 +0200

    Add files via upload

---
+++ skierka_zewnetrzny.js
@@ -0,0 +1,5 @@
+document.write("mój numer w dzienniku to 22"+"<br>");
+document.write("pierwszy wiersz to: 8 tak tak 7"+"<br>");
+document.write("drugi wiersz to: 8 tak tak 7"+"<br>");
+document.write("ostatnia modyfikacja strony".fontcolor("fuchsia").bold().strike().fontsize(7)+"<br>");
+document.write(document.lastModified.fontcolor("fuchsia").bold().strike().fontsize(7)+"<br>");
